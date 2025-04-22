f7fa661520c09acd67bf4e6be7e5c54368e9f606
CagataySavasli <cagatay.savasli@ozu.edu.tr>
2025-04-22 19:16:27 +0300

    Fix the classifier directory to resolve the model not found issue

--- tests/import_test.py
+++ tests/import_test.py
@@ -1,4 +1,6 @@
 import gymnasium as gym
 import reveal_mnist
 
-env = gym.make('RevealMNIST-v0', classifier_model_weights_loc="/Users/emirarditi/reveal_mnist/utils/mnist_predictor_masked.pt", device='mps')
+env = gym.make('RevealMNIST-v0',
+               classifier_model_weights_loc="./mnist_predictor_masked.pt",
+               device='DEVICE_TO_USE', visualize=True) # the device should be "cpu", "gpu" or "mps" based on your config
--- tests/random_agent.py
+++ tests/random_agent.py
@@ -1,7 +1,10 @@
 import gymnasium as gym
 import reveal_mnist
 
-env = gym.make('RevealMNIST-v0', classifier_model_weights_loc="mnist_predictor_masked.pt", device='mps', visualize=True)
+env = gym.make('RevealMNIST-v0',
+               classifier_model_weights_loc="./mnist_predictor_masked.pt",
+               device='DEVICE_TO_USE', visualize=True) # the device should be "cpu", "gpu" or "mps" based on your config
+
 
 NUM_EPISODES = 1000
 
